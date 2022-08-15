c4df1438b684be100cd425ac2bbe628d555b4264
Kaan Turan <kaann.dev@gmail.com>
2022-08-15 20:13:49 +0300

    Initial Commit

---
+++ Face Reminder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Face Reminder
+//
+//  Created by Kaan Turan on 15.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
