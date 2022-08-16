3cdfd062fab8a694b856d735481d09cd198bccf3
Kaan Turan <kaann.dev@gmail.com>
2022-08-16 09:48:36 +0300

    Project has been start

---
+++ Face Reminder/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Face Reminder
+//
+//  Created by Kaan Turan on 16.08.2022.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+   
+    @IBOutlet var name: UILabel!
+    @IBOutlet var imageView: UIImageView!
+}
