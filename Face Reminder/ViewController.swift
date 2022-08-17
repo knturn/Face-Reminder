d7f67575a80b1286810b9c2c665300de0966d6c5
Kaan Turan <kaann.dev@gmail.com>
2022-08-17 09:43:50 +0300

    UserDefault added. Users can save and delete person

--- Face Reminder/ViewController.swift
+++ Face Reminder/ViewController.swift
@@ -11,7 +11,20 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
     var people = [Person]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(loadpPhoto))    }
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(loadpPhoto))
+        let defaults = UserDefaults.standard
+        
+        if let savedPeople = defaults.object(forKey: "Faceses") as? Data {
+            let jsonDecoder = JSONDecoder()
+            
+            do {
+                people = try jsonDecoder.decode([Person].self, from: savedPeople)
+            } catch {
+                print("Failed to load people")
+            }
+        }
+        
+    }
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         people.count
     }
@@ -23,11 +36,11 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         let person = people[indexPath.item]
         cell.name.text = person.name
         let path = getDocumentsDirectory().appendingPathComponent(person.image)
-         cell.imageView.image = UIImage(contentsOfFile: path.path)
+        cell.imageView.image = UIImage(contentsOfFile: path.path)
         cell.imageView.layer.borderColor = UIColor(white: 0, alpha: 0.3).cgColor
-            cell.imageView.layer.borderWidth = 2
-            cell.imageView.layer.cornerRadius = 3
-            cell.layer.cornerRadius = 7
+        cell.imageView.layer.borderWidth = 2
+        cell.imageView.layer.cornerRadius = 3
+        cell.layer.cornerRadius = 7
         return cell
     }
     
@@ -37,7 +50,7 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         if sourceType {
             picker.sourceType = .camera
         }
-            
+        
         picker.allowsEditing = true
         picker.delegate = self
         present(picker, animated: true)
@@ -52,20 +65,21 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         let person = Person(name: "Unknown", image: imageName)
         people.append(person)
         collectionView.reloadData()
+        save()
         dismiss(animated: true)
     }
     func getDocumentsDirectory() -> URL{
         let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-            return paths[0]
+        return paths[0]
     }
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let person = people[indexPath.item]
-
+        
         let ac = UIAlertController(title: "Who is?", message: nil, preferredStyle: .alert)
         ac.addTextField()
-
+        
         ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
-       
+        
         ac.addAction(UIAlertAction(title: "OK", style: .default) { [weak self, weak ac] _ in
             guard var newName = ac?.textFields?[0].text else { return }
             if  !newName.isEmpty {
@@ -74,19 +88,28 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
                 newName = "Still Unknown"
                 person.name = newName
             }
-                    
-            
-
             self?.collectionView.reloadData()
+            self?.save()
         })
         ac.addAction(UIAlertAction(title: "Delete this face", style: .default) {
             [weak self] _ in
             
             self?.people.remove(at: indexPath.item)
-            collectionView.reloadData()
+            self?.save()
+            self?.collectionView.reloadData()
+            
         })
         present(ac, animated: true)
     }
-
+    func save() {
+        let jsonEncoder = JSONEncoder()
+        if let savedData = try? jsonEncoder.encode(people) {
+            let defaults = UserDefaults.standard
+            defaults.set(savedData, forKey: "Faceses")
+        } else {
+            print("Failed to save people.")
+        }
+    }
+    
 }
 
